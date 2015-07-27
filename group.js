b305402f451ed7ffa807448c5e77936797de463b
sephcoster <sephcoster@gmail.com>
2015-07-26 23:59:19 -0400

    updated to add CL param

--- group.js
+++ group.js
@@ -7,9 +7,10 @@ var data = JSON.parse(fs.readFileSync('layTitles.json', 'utf8'));
 
 // Adjust this configuration to loop through your file properly
 var config = {
-  parentField: 'O*NET-SOC Title', // The field you want to group by
-  otherParentFields: ['O*NET-SOC Code'], // Other fields you'd like to include in the new object
-  relatedField: 'Lay Title' // The field you would like to push to a new, combined array
+  parentField: 'O*NET-SOC Title', // The field you are grouping by
+  otherParentFields: ['O*NET-SOC Code'], // Other fields you'd like to include alongside the grouping
+  relatedField: 'Lay Title', // The field you would like to combine based on parentField
+  outputFileName:  process.argv[2] || 'groupCompleted.js' // Take an argument for Output File
 };
 
 var combined = { };
@@ -27,11 +28,11 @@ _.forEach(data, function(n, key) {
   combined[parent].related.push( n[config.relatedField] );
 });
 
-fs.writeFile("mergedCompleted.js", JSON.stringify(combined), function(err) {
+fs.writeFile(config.outputFileName, JSON.stringify(combined), function(err) {
     if(err) {
         return console.log(err);
     }
 
-    console.log("The file was saved as mergedCompleted.js");
+    console.log('The file was saved as ' + config.outputFileName);
 });
 
